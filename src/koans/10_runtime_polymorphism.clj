03e45bc1bc8db6c3c8a9c9543f2bb8dda7da5379
PJ Hampton <pjhampton@protonmail.com>
2017-09-04 10:39:04 +0100

    Finish runtim polymorphism

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -10,9 +10,9 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :herbivore [a] (str (:name a) " eats veggies."))
+(defmethod diet :carnivore [a] (str (:name a) " eats animals."))
+(defmethod diet :default [a] (str "I don't know what " (:name a) " eats."))
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
@@ -22,7 +22,7 @@
   (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= "Hello to this group: Peter, Paul, Mary" 
+  (= "Hello to this group: Peter, Paul, Mary!" 
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
